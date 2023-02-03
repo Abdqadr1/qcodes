32cd90a24123085ed107640e5184c98b1ea79267
Abdqadr1 <abolarinwaquadri@gmail.com>
2023-02-03 10:28:08 +0100

    delete old notifications scheduler

--- app/Invocables/DeleteReadNotification.php
+++ app/Invocables/DeleteReadNotification.php
@@ -8,6 +8,13 @@ class DeleteReadNotification
 {
     public function __invoke()
     {
-        Notification::where('type', 'GENERAL')->delete();
+        Notification::where(function ($query) {
+            $query->where('type', 'GENERAL');
+            $query->where('created_at', '<=', now()->subWeek());
+        })->orWhere(function ($q) {
+            $q->where('type', '!=', 'GENERAL');
+            $q->where('read', 1);
+            $q->where('created_at', '<=', now()->subDays(3));
+        })->delete();
     }
 }
