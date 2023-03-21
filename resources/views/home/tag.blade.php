ae7c15f9befef95a6aff3ac7310efe009e893fc1
Abdqadr1 <abolarinwaquadri@gmail.com>
2023-03-21 20:15:44 +0100

    category and tag columns

--- resources/views/home/tag.blade.php
+++ resources/views/home/tag.blade.php
@@ -12,24 +12,27 @@
                 {{$tag->content}}
             </div>
             <hr>
-            <div class="row justify-content-start gy-3">
+            
+             <div class="row justify-content-start gy-3">
                 @forelse ($articles as $article)
-                    <div class="col-md-4">
+                    <div class="col-md-4 d-flex" style="align-self: stretch;">
                         <div class="card article-card" style="width: 100%;">
                             {{-- <img src="{{ $article->banner }}" class="card-img-top" alt="{{ $article->title }}"> --}}
                             <div class="card-body pb-0">
-                                <h5 class="card-title">{{ $article->title }}</h5>
-                                <p class="card-text">{{ $article->summary }}</p>
+                                <h5 class="card-title fw-bold">{{ $article->title }}</h5>
+                                <p class="card-text ellipsis">{{ $article->summary }}</p>
                                 <p class="text-end">
-                                    <a href="{{ route('viewArticle', $article->slug) }}" class="text-end btn btn-danger btn-sm">Read more...</a>
+                                    <a href="{{ route('viewArticle', $article->slug) }}" class="text-end btn btn-danger btn-sm">Read more</a>
                                 </p>
                             </div>
                         </div>
                     </div>
                 @empty
-                    <div>No articles found</div>
-                @endforelse 
+                    <div style="height: 200px;">No articles</div>
+                @endforelse
+                
             </div>
+
             <div class="d-flex justify-content-end mt-3">
                 {{ $articles->links() }} 
             </div>
